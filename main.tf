3311d90c4eecef33a8fc2925a0675a3f47b99af4
Janpreet Singh <janpreetsinghgill@gmail.com>
2024-08-17 10:30:52 -0400

    add more details

--- main.tf
+++ main.tf
@@ -5,11 +5,13 @@ resource "null_resource" "check_vm" {
 
   provisioner "local-exec" {
     command = <<EOT
-      result=$(curl -s -k -d "username=${var.pm_user}&password=${var.pm_password}" ${var.pm_api_url}/access/ticket | jq -r '.data.ticket' | xargs -I {} curl -s -k -H "Authorization: PVEAuthCookie={}" ${var.pm_api_url}/nodes/${var.node_name}/qemu/${var.vm_id}/status/current | jq -r '.data.status')
+      result=$(curl -s -k -d "username=${var.pm_user}&password=${var.pm_password}" ${var.pm_api_url}/access/ticket | jq -r '.data.ticket' | xargs -I {} curl -s -k -H "Authorization: PVEAuthCookie={}" ${var.pm_api_url}/nodes/${var.node_name}/qemu/${var.vm_id}/status/current | jq -r '.data')
       if [ "$result" != "null" ]; then
-        echo "true" > ${var.vm_exists_file_path}
+        echo "exists=true" > ${var.vm_exists_file_path}
+        echo "vm_status=$(echo $result | jq -r '.status')" >> ${var.vm_exists_file_path}
+        echo "vm_name=$(echo $result | jq -r '.name')" >> ${var.vm_exists_file_path}
       else
-        echo "false" > ${var.vm_exists_file_path}
+        echo "exists=false" > ${var.vm_exists_file_path}
       fi
     EOT
   }
